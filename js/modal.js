bb8b4db590421af815050dbc1504b92b62d4dc88
Chris Donofrio <cdonofrio20@gmail.com>
2015-11-30 13:07:51 -0500

    Added script to hide listing / show search results

--- js/modal.js
+++ js/modal.js
@@ -3,3 +3,23 @@
 //     $("[data-target='#myModal1']").fadeTo("slow", 0.25);
 //   });
 // });
+
+$(document).ready(function() {
+	$(".row-delete").hide();
+
+  $(".hide-listing").on("click", function() {
+      $(this).closest(".row-delete").remove();
+      $(".modal-backdrop").hide();
+  });
+
+$(".btn-success").click(function(e) {
+	e.preventDefault();
+  var location = $("#location").val();
+  if (location !== "") {
+  	$(".row-delete").show();
+    }
+});
+
+
+
+});
